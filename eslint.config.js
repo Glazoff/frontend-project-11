813a5aec354db00fc855f586181a4b273109e468
Glazoff <glazov_evgen@mail.ru>
2026-01-24 16:45:09 +0300

    fix_eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,17 @@
-import globals from 'globals';
-import js from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import { includeIgnoreFile } from '@eslint/compat'
+import stylistic from '@stylistic/eslint-plugin'
+import { fileURLToPath } from 'url'
+
+const gitIgnorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+const eslintIgnorePath = fileURLToPath(new URL('.eslintignore', import.meta.url))
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/semi': ['error', 'always'],
-      '@stylistic/semi-style': ['error', 'last'], 
-      '@stylistic/semi-spacing': ['error', { 
-        before: false,
-        after: true,
-      }],
-      
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/comma-dangle': ['error', 'always-multiline'],
-      '@stylistic/comma-spacing': ['error', { before: false, after: true }],
-      'no-console': 'warn',
-      'no-unused-vars': 'warn',
-    },
-  },
-  js.configs.recommended,
-]);
+  includeIgnoreFile(gitIgnorePath),
+  includeIgnoreFile(eslintIgnorePath),
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: { ...globals.browser, process: 'readonly' } } },
+])
